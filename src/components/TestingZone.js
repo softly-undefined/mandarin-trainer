c0361059143701745f9c0a7d45e8eef1f1564a56
colin <42125428+colinhartigan@users.noreply.github.com>
2023-08-03 17:55:27 -0400

    consolidate event listener

--- src/components/TestingZone.js
+++ src/components/TestingZone.js
@@ -1,11 +1,5 @@
-import { useState, useEffect, useMemo, useCallback, useRef } from "react";
-import {
-    Stack,
-    Card,
-    Form,
-    Button,
-    CloseButton,
-} from "react-bootstrap";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { Button, Card, CloseButton, Form, Stack } from "react-bootstrap";
 import data from "./dictionary.json";
 
 export default function Menu(props) {
@@ -21,7 +15,7 @@ export default function Menu(props) {
         setIsMultipleChoice,
         responseCounts,
         setResponseCounts,
-        ProgressBar
+        ProgressBar,
     } = props; //should probably change this to get only given, want, and set because we dont need to change them
 
     const [term, setTerm] = useState(""); //change this initial value
@@ -43,8 +37,6 @@ export default function Menu(props) {
     const [buttonState, setButtonState] = useState("primary");
     const [buttonText, setButtonText] = useState("SUBMIT");
 
-
-
     const [cardWidth, setCardWidth] = useState(400); //doesn't change right now but can add functionality eventually
 
     const formStyle = {
@@ -64,7 +56,6 @@ export default function Menu(props) {
     //progress bar
     //const [remainingSetLength, setRemainingSetLength] = useState(0);
 
-    
     //multiple choice related variables
 
     const [answer1, setAnswer1] = useState("");
@@ -72,37 +63,27 @@ export default function Menu(props) {
     const [answer3, setAnswer3] = useState("");
     const [answer4, setAnswer4] = useState("");
 
-    const button1Ref = useRef()
-    const button2Ref = useRef()
-    const button3Ref = useRef()
-    const button4Ref = useRef()
-
+    const button1Ref = useRef();
+    const button2Ref = useRef();
+    const button3Ref = useRef();
+    const button4Ref = useRef();
 
     const [mcAnswerChoice, setMCAnswerChoice] = useState(4);
     const [answerPlacement, setAnswerPlacement] = useState(4);
     const [mcCorrect, setMCCorrect] = useState(false);
     const [mcButtonSize, setMCButtonSize] = useState(10); //rn i just hardwired the button minwidth but it should be variable around cardsize eventually
 
-    const handleButton1 = (event) => {
-        if(event.keyCode === 49) {
+    const handleButton = (event) => {
+        if (event.keyCode === 49) {
             button1Ref.current.click();
-        }
-    }
-    const handleButton2 = (event) => {
-        if(event.keyCode === 50) {
+        } else if (event.keyCode === 50) {
             button2Ref.current.click();
-        }
-    }
-    const handleButton3 = (event) => {
-        if(event.keyCode === 51) {
+        } else if (event.keyCode === 51) {
             button3Ref.current.click();
-        }
-    }
-    const handleButton4 = (event) => {
-        if(event.keyCode === 52) {
+        } else if (event.keyCode === 52) {
             button4Ref.current.click();
         }
-    }
+    };
 
     const assignMC = (currAns) => {
         const answerLocation = Math.floor(Math.random() * 4);
@@ -113,20 +94,20 @@ export default function Menu(props) {
         setAnswer4(selectRandomAnswer(currAns));
         // console.log(answerLocation); //debuggywuggy tools
         // console.log(currAns);
-        if(answerLocation === 0) {
+        if (answerLocation === 0) {
             setAnswer1(currAns);
             setAnswerPlacement(0);
-        }else if(answerLocation === 1) {
+        } else if (answerLocation === 1) {
             setAnswer2(currAns);
             setAnswerPlacement(1);
-        }else if(answerLocation === 2) {
+        } else if (answerLocation === 2) {
             setAnswer3(currAns);
             setAnswerPlacement(2);
-        }else if(answerLocation === 3) {
+        } else if (answerLocation === 3) {
             setAnswer4(currAns);
             setAnswerPlacement(3);
         }
-    }
+    };
 
     // const getMCButtonStyle = (length) => {
     //     //do some calculations here about length
@@ -134,18 +115,21 @@ export default function Menu(props) {
     //     minWidth: mcButtonSize,
     // }
     const getFontSize = (length) => {
-        return length > 5 ? '25px' : '50px';
-    }
+        return length > 5 ? "25px" : "50px";
+    };
 
     const selectRandomAnswer = (currAns) => {
-        if(trainingSet.words.length > 4) {
+        if (trainingSet.words.length > 4) {
             const randomIndex = Math.floor(Math.random() * trainingSet.words.length);
-            return trainingSet.words[randomIndex][want] === currAns ? selectRandomAnswer(currAns) : trainingSet.words[randomIndex][want];
+            return trainingSet.words[randomIndex][want] === currAns
+                ? selectRandomAnswer(currAns)
+                : trainingSet.words[randomIndex][want];
             //right now two of the dumb answers could be in the multiple choice answers
         }
-    }
+    };
 
-    const shuffle = (set) => { //takes an array and shuffles the stuff inside
+    const shuffle = (set) => {
+        //takes an array and shuffles the stuff inside
         if (set.words.length > 0) {
             let shuffled = set.words
                 .map((value) => ({ value, sort: Math.random() }))
@@ -168,7 +152,7 @@ export default function Menu(props) {
         setRemainingSet(nextSet);
         setTerm(next[given]);
         setKey(next[want]);
-        if(isMultipleChoice){
+        if (isMultipleChoice) {
             assignMC(next[want]); //assigns the multiple choice answers/questions
         }
         setCurrChar(next["character"]);
@@ -185,7 +169,10 @@ export default function Menu(props) {
             let newTrainingSet = { ...trainingSet };
             newTrainingSet.words = newTrainingSet.words.filter((word) => {
                 //this area iterates through the set adding ones in that shouldnt be removed
-                return ((answerCounts[word[want]] || 0) < 3) && !(((answerCounts[word[want]] || 0) >= 2) && ((wrongCounts[word[want]] || 0) === 0));
+                return (
+                    (answerCounts[word[want]] || 0) < 3 &&
+                    !((answerCounts[word[want]] || 0) >= 2 && (wrongCounts[word[want]] || 0) === 0)
+                );
             });
             console.log(newTrainingSet);
             shuffle(newTrainingSet);
@@ -203,20 +190,14 @@ export default function Menu(props) {
     }, [trainingSet]);
 
     useEffect(() => {
-        document.addEventListener('keydown', handleButton1);
-        document.addEventListener('keydown', handleButton2);
-        document.addEventListener('keydown', handleButton3);
-        document.addEventListener('keydown', handleButton4);
+        document.addEventListener("keydown", handleButton);
         return () => {
-            document.removeEventListener('keydown', handleButton1);
-            document.removeEventListener('keydown', handleButton2);
-            document.removeEventListener('keydown', handleButton3);
-            document.removeEventListener('keydown', handleButton4);
+            document.removeEventListener("keydown", handleButton);
         };
     }, []);
 
     const handleSubmit = (event) => {
-        if(!isMultipleChoice){
+        if (!isMultipleChoice) {
             event.preventDefault();
         }
         //when this happens {answer} will be set to the persons answer so here is where handle a "submission"
@@ -238,23 +219,22 @@ export default function Menu(props) {
             setTerm("");
             setIsSubmitting(true);
 
-
             //we don't setAnswer here because not sure if want to leave persons answer in the textbox
-            if (answer === key || (event === answerPlacement)) {
-                if(isMultipleChoice) {
-                    if(answerPlacement === 0) {
+            if (answer === key || event === answerPlacement) {
+                if (isMultipleChoice) {
+                    if (answerPlacement === 0) {
                         setAnswer2("CORRECT!");
                         setAnswer3("CORRECT!");
                         setAnswer4("CORRECT!");
-                    }else if (answerPlacement === 1) {
+                    } else if (answerPlacement === 1) {
                         setAnswer1("CORRECT!");
                         setAnswer3("CORRECT!");
                         setAnswer4("CORRECT!");
-                    }else if (answerPlacement === 2) {
+                    } else if (answerPlacement === 2) {
                         setAnswer1("CORRECT!");
                         setAnswer2("CORRECT!");
                         setAnswer4("CORRECT!");
-                    }else if (answerPlacement === 3) {
+                    } else if (answerPlacement === 3) {
                         setAnswer1("CORRECT!");
                         setAnswer2("CORRECT!");
                         setAnswer3("CORRECT!");
@@ -277,19 +257,19 @@ export default function Menu(props) {
 
                 //interact with the algorithm
             } else {
-                if(answerPlacement === 0) {
+                if (answerPlacement === 0) {
                     setAnswer2("INCORRECT!");
                     setAnswer3("INCORRECT!");
                     setAnswer4("INCORRECT!");
-                }else if (answerPlacement === 1) {
+                } else if (answerPlacement === 1) {
                     setAnswer1("INCORRECT!");
                     setAnswer3("INCORRECT!");
                     setAnswer4("INCORRECT!");
-                }else if (answerPlacement === 2) {
+                } else if (answerPlacement === 2) {
                     setAnswer1("INCORRECT!");
                     setAnswer2("INCORRECT!");
                     setAnswer4("INCORRECT!");
-                }else if (answerPlacement === 3) {
+                } else if (answerPlacement === 3) {
                     setAnswer1("INCORRECT!");
                     setAnswer2("INCORRECT!");
                     setAnswer3("INCORRECT!");
@@ -344,84 +324,112 @@ export default function Menu(props) {
                         <Stack gap={2}>
                             <Form.Label
                                 style={{
-                                    fontSize: getFontSize(term.length)/*would love to make this font size variable*/,
+                                    fontSize: getFontSize(term.length) /*would love to make this font size variable*/,
                                 }}
                             >
                                 {term}
                             </Form.Label>
                             <Form.Label
-                                
                                 style={{
-                                    fontSize:
-                                        "25px" /*same here but less necessary*/,
+                                    fontSize: "25px" /*same here but less necessary*/,
                                 }}
                             >
-                                {keyText &&
+                                {keyText && (
                                     <Stack>
                                         <Form.Label style={{ fontSize: "50px" }}>{currChar}</Form.Label>
                                         <Form.Label>{currPinyin}</Form.Label>
                                         <Form.Label>{currDefinition}</Form.Label>
                                     </Stack>
-                                }
+                                )}
                             </Form.Label>
 
-                            { !isMultipleChoice && 
+                            {!isMultipleChoice && (
                                 <Form.Control
-                                style={formStyle}
-                                spellCheck='false'
-                                placeholder='Answer'
-                                id='responseArea'
-                                readOnly={isSubmitting}
-                                type='text'
-                                value={answer}
-                                onChange={(event) =>
-                                    setAnswer(event.target.value)
-                                }
+                                    style={formStyle}
+                                    spellCheck='false'
+                                    placeholder='Answer'
+                                    id='responseArea'
+                                    readOnly={isSubmitting}
+                                    type='text'
+                                    value={answer}
+                                    onChange={(event) => setAnswer(event.target.value)}
                                 />
-                            }
+                            )}
                         </Stack>
-                        { !isMultipleChoice &&
-                        <Button type='submit' variant={buttonState}>
-                            {buttonText}
-                        </Button>
-                        }
-                        { isMultipleChoice &&
-                        <Stack
-                        direction='horizontal'
-                        gap={1}
-                        style={{
-                            justifyContent: "flex-start",
-                            alignItems: "center",
-                        }}
-                        >
+                        {!isMultipleChoice && (
+                            <Button type='submit' variant={buttonState}>
+                                {buttonText}
+                            </Button>
+                        )}
+                        {isMultipleChoice && (
                             <Stack
-                                direction='vertical'
+                                direction='horizontal'
                                 gap={1}
                                 style={{
                                     justifyContent: "flex-start",
                                     alignItems: "center",
-                            }}
-                            >       
-                                <Button style={{fontSize:'20px', minWidth: '165px'}} variant={buttonState} ref={button1Ref} onClick={() => {handleSubmit(0) }}>{answer1}</Button>
-                                <Button style={{fontSize:'20px', minWidth: '165px'}} variant={buttonState} ref={button3Ref} onClick={() => {handleSubmit(1) }}>{answer2}</Button>
+                                }}
+                            >
+                                <Stack
+                                    direction='vertical'
+                                    gap={1}
+                                    style={{
+                                        justifyContent: "flex-start",
+                                        alignItems: "center",
+                                    }}
+                                >
+                                    <Button
+                                        style={{ fontSize: "20px", minWidth: "165px" }}
+                                        variant={buttonState}
+                                        ref={button1Ref}
+                                        onClick={() => {
+                                            handleSubmit(0);
+                                        }}
+                                    >
+                                        {answer1}
+                                    </Button>
+                                    <Button
+                                        style={{ fontSize: "20px", minWidth: "165px" }}
+                                        variant={buttonState}
+                                        ref={button3Ref}
+                                        onClick={() => {
+                                            handleSubmit(1);
+                                        }}
+                                    >
+                                        {answer2}
+                                    </Button>
+                                </Stack>
+                                <Stack
+                                    direction='vertical'
+                                    gap={1}
+                                    style={{
+                                        justifyContent: "flex-start",
+                                        alignItems: "center",
+                                    }}
+                                >
+                                    <Button
+                                        style={{ fontSize: "20px", minWidth: "165px" }}
+                                        variant={buttonState}
+                                        ref={button2Ref}
+                                        onClick={() => {
+                                            handleSubmit(2);
+                                        }}
+                                    >
+                                        {answer3}
+                                    </Button>
+                                    <Button
+                                        style={{ fontSize: "20px", minWidth: "165px" }}
+                                        variant={buttonState}
+                                        ref={button4Ref}
+                                        onClick={() => {
+                                            handleSubmit(3);
+                                        }}
+                                    >
+                                        {answer4}
+                                    </Button>
+                                </Stack>
                             </Stack>
-                            <Stack
-                                direction='vertical'
-                                gap={1}
-                                style={{
-                                    justifyContent: "flex-start",
-                                    alignItems: "center",
-                            }}
-                            >       
-                                <Button style={{fontSize:'20px', minWidth: '165px'}} variant={buttonState} ref={button2Ref} onClick={() => {handleSubmit(2) }}>{answer3}</Button>
-                                <Button style={{fontSize:'20px', minWidth: '165px'}} variant={buttonState} ref={button4Ref} onClick={() => {handleSubmit(3) }}>{answer4}</Button>
-                            </Stack>
-                        </Stack>
-                        
-                        }
-                        
-                        
-                        
+                        )}
                     </Stack>
                 </Form>
             </Card.Body>
